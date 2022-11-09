a927fd824b14182244e49248f175cb1b3c105ba4
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-11-08 20:30:22 -0800

    Updated From Class

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,8 +41,29 @@
             this.rtbEnroll = new System.Windows.Forms.RichTextBox();
             this.tbName = new System.Windows.Forms.TextBox();
             this.btnEnroll = new System.Windows.Forms.Button();
+            this.btnShowPizzaStuff = new System.Windows.Forms.Button();
+            this.chkPepperoni = new System.Windows.Forms.CheckBox();
+            this.chkXCheese = new System.Windows.Forms.CheckBox();
+            this.chkPineapple = new System.Windows.Forms.CheckBox();
+            this.chkMushrooms = new System.Windows.Forms.CheckBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.rbSmall = new System.Windows.Forms.RadioButton();
+            this.rbMedium = new System.Windows.Forms.RadioButton();
+            this.rbXL = new System.Windows.Forms.RadioButton();
+            this.rbLarge = new System.Windows.Forms.RadioButton();
+            this.gpPizzaSize = new System.Windows.Forms.GroupBox();
+            this.gpCrust = new System.Windows.Forms.GroupBox();
+            this.rbRegular = new System.Windows.Forms.RadioButton();
+            this.rbStuffed = new System.Windows.Forms.RadioButton();
+            this.rbGarlic = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtPizzaName = new System.Windows.Forms.TextBox();
             this.grpTerm.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.gpPizzaSize.SuspendLayout();
+            this.gpCrust.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpTerm
@@ -52,7 +73,7 @@
             this.grpTerm.Controls.Add(this.rbSpring);
             this.grpTerm.Controls.Add(this.rbWinter);
             this.grpTerm.Controls.Add(this.rbFall);
-            this.grpTerm.Location = new System.Drawing.Point(139, 33);
+            this.grpTerm.Location = new System.Drawing.Point(29, 33);
             this.grpTerm.Name = "grpTerm";
             this.grpTerm.Size = new System.Drawing.Size(161, 157);
             this.grpTerm.TabIndex = 0;
@@ -150,29 +171,29 @@
             this.panel1.Controls.Add(this.chkDatabase);
             this.panel1.Controls.Add(this.chkWeb);
             this.panel1.Controls.Add(this.chkGame);
-            this.panel1.Location = new System.Drawing.Point(319, 33);
+            this.panel1.Location = new System.Drawing.Point(209, 33);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(217, 210);
             this.panel1.TabIndex = 5;
             // 
             // rtbEnroll
             // 
-            this.rtbEnroll.Location = new System.Drawing.Point(559, 33);
+            this.rtbEnroll.Location = new System.Drawing.Point(489, 33);
             this.rtbEnroll.Name = "rtbEnroll";
-            this.rtbEnroll.Size = new System.Drawing.Size(217, 324);
+            this.rtbEnroll.Size = new System.Drawing.Size(439, 214);
             this.rtbEnroll.TabIndex = 6;
             this.rtbEnroll.Text = "";
             // 
             // tbName
             // 
-            this.tbName.Location = new System.Drawing.Point(139, 208);
+            this.tbName.Location = new System.Drawing.Point(29, 208);
             this.tbName.Name = "tbName";
             this.tbName.Size = new System.Drawing.Size(159, 26);
             this.tbName.TabIndex = 7;
             // 
             // btnEnroll
             // 
-            this.btnEnroll.Location = new System.Drawing.Point(233, 261);
+            this.btnEnroll.Location = new System.Drawing.Point(123, 261);
             this.btnEnroll.Name = "btnEnroll";
             this.btnEnroll.Size = new System.Drawing.Size(156, 32);
             this.btnEnroll.TabIndex = 8;
@@ -180,11 +201,197 @@
             this.btnEnroll.UseVisualStyleBackColor = true;
             this.btnEnroll.Click += new System.EventHandler(this.btnEnroll_Click);
             // 
+            // btnShowPizzaStuff
+            // 
+            this.btnShowPizzaStuff.Location = new System.Drawing.Point(309, 553);
+            this.btnShowPizzaStuff.Name = "btnShowPizzaStuff";
+            this.btnShowPizzaStuff.Size = new System.Drawing.Size(117, 40);
+            this.btnShowPizzaStuff.TabIndex = 10;
+            this.btnShowPizzaStuff.Text = "Make Pizza";
+            this.btnShowPizzaStuff.UseVisualStyleBackColor = true;
+            this.btnShowPizzaStuff.Click += new System.EventHandler(this.btnShowPizzaStuff_Click);
+            // 
+            // chkPepperoni
+            // 
+            this.chkPepperoni.AutoSize = true;
+            this.chkPepperoni.Location = new System.Drawing.Point(47, 30);
+            this.chkPepperoni.Name = "chkPepperoni";
+            this.chkPepperoni.Size = new System.Drawing.Size(160, 24);
+            this.chkPepperoni.TabIndex = 11;
+            this.chkPepperoni.Text = "Pepperoni - $1.50";
+            this.chkPepperoni.UseVisualStyleBackColor = true;
+            // 
+            // chkXCheese
+            // 
+            this.chkXCheese.AutoSize = true;
+            this.chkXCheese.Location = new System.Drawing.Point(47, 64);
+            this.chkXCheese.Name = "chkXCheese";
+            this.chkXCheese.Size = new System.Drawing.Size(184, 24);
+            this.chkXCheese.TabIndex = 12;
+            this.chkXCheese.Text = "Extra Cheese - $1.00";
+            this.chkXCheese.UseVisualStyleBackColor = true;
+            // 
+            // chkPineapple
+            // 
+            this.chkPineapple.AutoSize = true;
+            this.chkPineapple.Location = new System.Drawing.Point(47, 98);
+            this.chkPineapple.Name = "chkPineapple";
+            this.chkPineapple.Size = new System.Drawing.Size(149, 24);
+            this.chkPineapple.TabIndex = 13;
+            this.chkPineapple.Text = "Pineapple $4.00";
+            this.chkPineapple.UseVisualStyleBackColor = true;
+            // 
+            // chkMushrooms
+            // 
+            this.chkMushrooms.AutoSize = true;
+            this.chkMushrooms.Location = new System.Drawing.Point(47, 132);
+            this.chkMushrooms.Name = "chkMushrooms";
+            this.chkMushrooms.Size = new System.Drawing.Size(171, 24);
+            this.chkMushrooms.TabIndex = 14;
+            this.chkMushrooms.Text = "Mushrooms - $1.00";
+            this.chkMushrooms.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Salmon;
+            this.panel2.Controls.Add(this.chkPepperoni);
+            this.panel2.Controls.Add(this.chkMushrooms);
+            this.panel2.Controls.Add(this.chkXCheese);
+            this.panel2.Controls.Add(this.chkPineapple);
+            this.panel2.Location = new System.Drawing.Point(29, 333);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(271, 209);
+            this.panel2.TabIndex = 15;
+            // 
+            // rbSmall
+            // 
+            this.rbSmall.AutoSize = true;
+            this.rbSmall.Location = new System.Drawing.Point(22, 30);
+            this.rbSmall.Name = "rbSmall";
+            this.rbSmall.Size = new System.Drawing.Size(135, 24);
+            this.rbSmall.TabIndex = 16;
+            this.rbSmall.TabStop = true;
+            this.rbSmall.Text = "Small - $10.00";
+            this.rbSmall.UseVisualStyleBackColor = true;
+            // 
+            // rbMedium
+            // 
+            this.rbMedium.AutoSize = true;
+            this.rbMedium.Location = new System.Drawing.Point(21, 78);
+            this.rbMedium.Name = "rbMedium";
+            this.rbMedium.Size = new System.Drawing.Size(152, 24);
+            this.rbMedium.TabIndex = 17;
+            this.rbMedium.TabStop = true;
+            this.rbMedium.Text = "Medium - $13.00";
+            this.rbMedium.UseVisualStyleBackColor = true;
+            // 
+            // rbXL
+            // 
+            this.rbXL.AutoSize = true;
+            this.rbXL.Location = new System.Drawing.Point(23, 174);
+            this.rbXL.Name = "rbXL";
+            this.rbXL.Size = new System.Drawing.Size(178, 24);
+            this.rbXL.TabIndex = 19;
+            this.rbXL.TabStop = true;
+            this.rbXL.Text = "Extra Large - $18.00";
+            this.rbXL.UseVisualStyleBackColor = true;
+            // 
+            // rbLarge
+            // 
+            this.rbLarge.AutoSize = true;
+            this.rbLarge.Location = new System.Drawing.Point(23, 126);
+            this.rbLarge.Name = "rbLarge";
+            this.rbLarge.Size = new System.Drawing.Size(141, 24);
+            this.rbLarge.TabIndex = 18;
+            this.rbLarge.TabStop = true;
+            this.rbLarge.Text = "Large - $ 15.00";
+            this.rbLarge.UseVisualStyleBackColor = true;
+            // 
+            // gpPizzaSize
+            // 
+            this.gpPizzaSize.Controls.Add(this.rbSmall);
+            this.gpPizzaSize.Controls.Add(this.rbXL);
+            this.gpPizzaSize.Controls.Add(this.rbMedium);
+            this.gpPizzaSize.Controls.Add(this.rbLarge);
+            this.gpPizzaSize.Location = new System.Drawing.Point(343, 306);
+            this.gpPizzaSize.Name = "gpPizzaSize";
+            this.gpPizzaSize.Size = new System.Drawing.Size(221, 241);
+            this.gpPizzaSize.TabIndex = 20;
+            this.gpPizzaSize.TabStop = false;
+            this.gpPizzaSize.Text = "Pizza Size";
+            // 
+            // gpCrust
+            // 
+            this.gpCrust.Controls.Add(this.rbGarlic);
+            this.gpCrust.Controls.Add(this.rbStuffed);
+            this.gpCrust.Controls.Add(this.rbRegular);
+            this.gpCrust.Location = new System.Drawing.Point(614, 306);
+            this.gpCrust.Name = "gpCrust";
+            this.gpCrust.Size = new System.Drawing.Size(305, 169);
+            this.gpCrust.TabIndex = 21;
+            this.gpCrust.TabStop = false;
+            this.gpCrust.Text = "Crust";
+            // 
+            // rbRegular
+            // 
+            this.rbRegular.AutoSize = true;
+            this.rbRegular.Location = new System.Drawing.Point(32, 30);
+            this.rbRegular.Name = "rbRegular";
+            this.rbRegular.Size = new System.Drawing.Size(143, 24);
+            this.rbRegular.TabIndex = 0;
+            this.rbRegular.TabStop = true;
+            this.rbRegular.Text = "Regular - $0.00";
+            this.rbRegular.UseVisualStyleBackColor = true;
+            // 
+            // rbStuffed
+            // 
+            this.rbStuffed.AutoSize = true;
+            this.rbStuffed.Location = new System.Drawing.Point(32, 78);
+            this.rbStuffed.Name = "rbStuffed";
+            this.rbStuffed.Size = new System.Drawing.Size(140, 24);
+            this.rbStuffed.TabIndex = 1;
+            this.rbStuffed.TabStop = true;
+            this.rbStuffed.Text = "Stuffed - $3.00";
+            this.rbStuffed.UseVisualStyleBackColor = true;
+            // 
+            // rbGarlic
+            // 
+            this.rbGarlic.AutoSize = true;
+            this.rbGarlic.Location = new System.Drawing.Point(32, 124);
+            this.rbGarlic.Name = "rbGarlic";
+            this.rbGarlic.Size = new System.Drawing.Size(128, 24);
+            this.rbGarlic.TabIndex = 2;
+            this.rbGarlic.TabStop = true;
+            this.rbGarlic.Text = "Garlic - $1.50";
+            this.rbGarlic.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtPizzaName);
+            this.groupBox1.Location = new System.Drawing.Point(614, 494);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(305, 109);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Infomation";
+            // 
+            // txtPizzaName
+            // 
+            this.txtPizzaName.Location = new System.Drawing.Point(32, 48);
+            this.txtPizzaName.Name = "txtPizzaName";
+            this.txtPizzaName.Size = new System.Drawing.Size(242, 26);
+            this.txtPizzaName.TabIndex = 0;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(972, 651);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gpCrust);
+            this.Controls.Add(this.gpPizzaSize);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnShowPizzaStuff);
             this.Controls.Add(this.btnEnroll);
             this.Controls.Add(this.tbName);
             this.Controls.Add(this.rtbEnroll);
@@ -196,6 +403,14 @@
             this.grpTerm.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.gpPizzaSize.ResumeLayout(false);
+            this.gpPizzaSize.PerformLayout();
+            this.gpCrust.ResumeLayout(false);
+            this.gpCrust.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -216,6 +431,23 @@
         private System.Windows.Forms.RichTextBox rtbEnroll;
         private System.Windows.Forms.TextBox tbName;
         private System.Windows.Forms.Button btnEnroll;
+        private System.Windows.Forms.Button btnShowPizzaStuff;
+        private System.Windows.Forms.CheckBox chkPepperoni;
+        private System.Windows.Forms.CheckBox chkXCheese;
+        private System.Windows.Forms.CheckBox chkPineapple;
+        private System.Windows.Forms.CheckBox chkMushrooms;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton rbSmall;
+        private System.Windows.Forms.RadioButton rbMedium;
+        private System.Windows.Forms.RadioButton rbXL;
+        private System.Windows.Forms.RadioButton rbLarge;
+        private System.Windows.Forms.GroupBox gpPizzaSize;
+        private System.Windows.Forms.GroupBox gpCrust;
+        private System.Windows.Forms.RadioButton rbGarlic;
+        private System.Windows.Forms.RadioButton rbStuffed;
+        private System.Windows.Forms.RadioButton rbRegular;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPizzaName;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,9 +15,167 @@ namespace Lecture_12_RadioButtons_CheckBoxes
         public Form1()
         {
             InitializeComponent();
+            // CHeckboxes
+            // Panel
+            PizzaDefault();
+
+
         }
 
+        public void PizzaDefault()
+        {
+            // Sets rbMedium to true, selecting it
+            rbMedium.Checked = true;
+            rbRegular.Checked = true;
+        }
 
+        private void btnShowPizzaStuff_Click(object sender, EventArgs e)
+        {
+            // Make sure to call your method
+            CalculatePizzaOrder();
+        }
+
+        // Holds the code to calculate our pizza order
+
+
+        public void CalculatePizzaOrder()
+        {
+            string pizzaSize, pizzaCrust;
+            string recipt = "Slice of Pi Pizza\n";
+            recipt += $"Name: {txtPizzaName.Text}\n";
+            recipt += $"---------------------------\n";
+            // Keeps track of our subsubTotal
+            double subTotal = 0;
+
+           
+
+            // Radio buttons
+            // Group together with a panel or group box for multiple sets of radio buttons
+
+            // To get the selected value use .checked on the radio button name
+
+            // Use an if / else if / else structure
+            if (rbSmall.Checked)
+            {
+                /*rtbEnroll.Text = "Pizza Size: Small";*/ // 10
+                subTotal += 10;
+                pizzaSize = "Small";
+            }
+            else if (rbMedium.Checked)
+            {
+                /*rtbEnroll.Text = "Pizza Size: Medium";*/ // 13
+                subTotal += 13;
+                pizzaSize = "Medium";
+            }
+            else if(rbLarge.Checked)
+            {
+                /*rtbEnroll.Text = "Pizza Size: Large";*/ // 15
+                subTotal += 15;
+                pizzaSize = "Large";
+
+            }
+            else 
+            {
+                /*rtbEnroll.Text = "Pizza Size: Extra Large";*/ // 18
+                subTotal += 18;
+                pizzaSize = "XL";
+            }
+
+            // Crusts
+            if (rbRegular.Checked)
+            {
+                subTotal += 0;
+                pizzaCrust = "Regular";
+            }
+            // Crusts
+            else if (rbStuffed.Checked)
+            {
+                subTotal += 3;
+                pizzaCrust = "Stuffed Crust";
+            }
+            // Crusts
+            else 
+            {
+                subTotal += 1.50;
+                pizzaCrust = "Garlic Crust";
+            }
+
+            // Displays pizza size and crust
+            recipt += $"{pizzaSize} {pizzaCrust} Pizza\n\n";
+            recipt += $"Toppings:\n";
+
+            // ----------------------------------------------
+
+            // Responds if a checkbox is checked
+            // Use .checked to return if a checkbox is check
+            if (chkPepperoni.Checked) // Pepperoni
+            {
+                double itemPrice = 2.50;
+                subTotal += itemPrice;
+                recipt += $"Pepperoni - {itemPrice.ToString("c")}\n";
+            }
+
+            if (chkMushrooms.Checked) // Mushrooms - 1
+            {
+                double itemPrice = 1;
+                subTotal += itemPrice;
+                recipt += $"Mushroom - {itemPrice.ToString("c")}\n";
+            }
+
+            if (chkPineapple.Checked) // Pineapple - 4
+            {
+                double itemPrice = 4;
+                subTotal += itemPrice;
+                recipt += $"Pineapple - {itemPrice.ToString("c")}\n";
+            }
+
+            if (chkXCheese.Checked) // Extra Cheese - 1
+            {
+                double itemPrice = 1;
+                subTotal += itemPrice;
+                recipt += $"Extra Cheese - {itemPrice.ToString("c")}\n";
+            }
+            // Checkboxes
+            // To access a checkbox, use the checkboxes name
+            //rtbEnroll.Text = chkPepperoni.Checked.ToString();
+
+            recipt += $"-----------------------------\n";
+            // Display Price Information
+            recipt += $"Subtotal: {subTotal.ToString("c")}\n";
+
+            double taxPercent = .1;
+            double taxAmount = CalculateTax(subTotal, taxPercent);
+
+            recipt += $"Tax: {taxPercent * 100}% - {taxAmount.ToString("c")}\n";
+
+            double finalTotal = subTotal + taxAmount;
+
+            recipt += $"Total: {finalTotal.ToString("c")}";
+
+            // DIsplays the subTotal amount of the pizza
+            rtbEnroll.Text = recipt;
+        }
+
+        // This method will take a subtotal, and a percent, and return a tax amount
+        public double CalculateTax(double subTotal, double taxPercent)
+        {
+
+            return subTotal * taxPercent;
+
+        } // CalculateTax()
+
+            /*
+            * Slice of PI Pizza
+            * Name: {Users Name}
+            * -------------------------------
+            * {Pizza Size} {Pizza Crust} Pizza
+            * Toppings:
+            * {LIst of toppings}
+            * --------------------------------
+            * SubsubTotal: {subsubTotal value}
+            * Tax: {dollar amount of tax}
+            * subTotal: {subTotal Costs}
+            */
 
         #region ExampleCode
         private void btnEnroll_Click(object sender, EventArgs e)
@@ -66,5 +224,7 @@ namespace Lecture_12_RadioButtons_CheckBoxes
 
         }
         #endregion ExampleCode
+
+
     }
 }
